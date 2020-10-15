bd4c9c16d577ac9288e827e419c7da7ffcd8e79f
consolejs <https200@126.com>
2020-10-15 17:07:16 +0800

    :fire: routeeer

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,18 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App" />
      -->
+    <p class="nav">
+        <router-link to="/foo">Go to Foo</router-link>
+        <router-link to="/bar">Go to Bar</router-link>
+    </p>
+    <p class="nav">
+        <router-link to="/user/1">/user/foo</router-link>
+        <router-link to="/user/2">/user/bar</router-link>
+    </p>
+    <div class="square">
+        <router-view></router-view>
+    </div>
+
     <Button />
     <Cell />
 </div>
@@ -11,6 +23,7 @@
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
+
 import Button from "./vants/Button.vue";
 import Cell from "./vants/Cell.vue";
 export default {
@@ -31,5 +44,20 @@ export default {
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
+}
+
+.nav a {
+    margin: 0 10px;
+}
+
+.router-link-active {
+    color: brown;
+}
+
+.square {
+    margin: 20px;
+    height: 60px;
+    line-height: 60px;
+    border: 1px solid green;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,50 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 import { Button } from 'vant';
 
+        
+const Foo = { template: '<div>foo1111</div>' }
+const Bar = { template: '<div>bar2222</div>' }
+
+const User = {
+  template: `<div>User: {{ $route.params.id }}</div>`,
+  watch: {
+    $route(to, from) {
+      // 对路由变化作出响应...
+      console.log('to:', to)
+      console.log('from:' , from)
+    },
+    // beforeRouteUpdate (to, from, next) {
+    //   // react to route changes...
+    //   // don't forget to call next()
+    //   console.log('to:', to)
+    //   console.log('from:' , from)
+    //   console.log('next:' , next)
+    // }
+  }
+}
+
+
+const routes = [
+  { path: '/foo', component: Foo },
+  { path: '/bar', component: Bar },
+  { path: '/user/:id', component: User },
+]
+
+const router = new VueRouter({
+  routes // (缩写) 相当于 routes: routes
+})
+
+
+
 Vue.use(Button);
+Vue.use(VueRouter);
 
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
